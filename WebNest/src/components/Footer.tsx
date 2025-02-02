ce0198d202c9019351a0990751cc137ba43d8ec7
Harsh Pratap Singh <harshpratapsingh.bt21aero@pec.edu.in>
2025-02-02 07:49:19 +0530

    Header done

---
+++ WebNest/src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <div>Hello From Footer</div>;
+};
---
+++ WebNest/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header";
+
+export const Home = () => {
+  return (
+    <div className="flex flex-col">
+      <Header />
+    </div>
+  );
+};
