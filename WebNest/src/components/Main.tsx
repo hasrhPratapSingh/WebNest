51202130487f1bf823beb0ee3ec49724b8a24b0f
Harsh Pratap Singh <harshpratapsingh.bt21aero@pec.edu.in>
2025-02-10 08:39:35 +0530

    background_image

--- WebNest/src/components/Main.tsx
+++ WebNest/src/components/Main.tsx
@@ -1,3 +1,24 @@
+import earth from "../assets/earth.jpg";
 export const Main = () => {
-  return <div className=" flex-1 pt-20"></div>;
+  return (
+    <div
+      className=" flex-1 pt-20 w-full"
+      style={{
+        backgroundImage: `url(${earth})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="grid place-content-center h-64 md:h-80 lg:h-96 ">
+        <div className="flex justify-evenly md:justify-center lg:justify-center text-xl md:text-3xl lg:text-4xl font-bold text-cyan-500 mb-3 md:mb-6 lg:mb-10 ">
+          Your Digital Success Starts Here
+        </div>
+        <div className="flex justify-center md:justify-center lg:justify-center text-white pl-10 pr-10 text-xl text-center">
+          At Webnest, we craft cutting-edge websites, dynamic mobile apps, and
+          impactful brand identities that drive growth. Let’s build something
+          amazing together!
+        </div>
+      </div>
+    </div>
+  );
 };
